1f17e82fe01a5a99e5bc22642a4571aeeff09b8e
vũ nguyễn tiến <nguyentienvu4897@gmail.com>
2023-02-20 16:28:08 +0700

    luattoanphuc

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
         $data['bannerAds'] = BannerAds::where('status',1)->get(['name','image','id']);
         $data['partners'] = Partner::where(['status'=>1])->get(['id','image','name','link']);
         $data['prizes'] = Prize::where(['status'=>1])->get();
-        $data['aboutUs'] = PageContent::where(['status'=>1, 'type'=>'gioi-thieu-chung', 'language'=>'vi'])->first();
+        $data['aboutUs'] = PageContent::where(['status'=>1, 'type'=>'gioi-thieu', 'language'=>'vi'])->first();
         $data['founders'] = Founder::where(['status'=>1])->get();
         $data['homeProduct'] = Product::where(['status'=>1, 'discountStatus'=>1])->limit(12)->get(['id','category','name','discount','price','images','slug','cate_slug','type_slug']);
         $data['homeBlog'] = Blog::where(['status'=>1, 'home_status'=>1])->orderBy('id', 'desc')->limit(4)->get(['id','title','image','description','created_at','updated_at','slug','category']);
